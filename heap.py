7fd1eb31328c7568c2c0d73f0a12191997aca1a1
taquang <danxawe@gmail.com>
2022-12-01 21:59:41 +0100

    update heap

--- heap.py
+++ heap.py
@@ -1,65 +1,63 @@
-class Heap:
+class Heep:
     def __init__(self):
-        self.heap = [0]
-    def printInternalArr(self):
-        output = ""
-        for item in self.heap:
-            output += str(item) + " "
-        print(output)
+        self.arr = [0]
+    def swap(self, i1, i2):
+        temp = self.arr[i2]
+        self.arr[i2] = self.arr[i1]
+        self.arr[i1] = temp
     def iterateUp(self, index):
-        while index//2 > 0:
-            if self.heap[index] < self.heap[index//2]:
-                break
-            temp = self.heap[index]
-            self.heap[index] = self.heap[index//2]
-            self.heap[index//2] = temp
-            index = index//2
-    def iterateDown(self, index):
-        while index*2+1 < len(self.heap):
-            nextIndex = index*2 if self.heap[index*2] > self.heap[index*2+1] else index*2+1
-            if self.heap[index] > self.heap[nextIndex]:
-                break
-            temp = self.heap[index]
-            self.heap[index] = self.heap[nextIndex]
-            self.heap[nextIndex] = temp
-            index = nextIndex
-
+        pIndex = index // 2
+        if self.arr[index] >= self.arr[pIndex]:
+            return
+        self.swap(index, pIndex)
+        self.iterateUp(pIndex)
     def push(self, val):
-        self.heap.append(val)
-        self.iterateUp(len(self.heap)-1)
+        i = len(self.arr)
+        self.arr.append(val)
+        self.iterateUp(i)
+    def iterateDown(self, index):
+        minChildIndex = 0
+        if index*2+1 <= len(self.arr)-1:
+            minChildIndex = index*2 if self.arr[index*2] < self.arr[index*2+1] else index*2+1
+        elif index*2 == len(self.arr)-1:
+            minChildIndex = index*2
+        if self.arr[index] < self.arr[minChildIndex] or minChildIndex == 0:
+            return
+        self.swap(index,minChildIndex)
+        self.iterateDown(minChildIndex)
     def pop(self):
-        if len(self.heap) <= 1:
+        if len(self.arr) <= 1:
             return None
-        if len(self.heap) == 2:
-            return self.heap.pop(len(self.heap)-1)
-        ret = self.heap[1]
-        self.heap[1] = self.heap.pop(len(self.heap)-1)
+        if len(self.arr) == 2:
+            return self.arr.pop()
+        ret = self.arr[1]
+        self.arr[1] = self.arr.pop()
         self.iterateDown(1)
         return ret
+        
+heep = Heep()
+heep.push(5)
+heep.push(7)
+heep.push(10)
+heep.push(1)
+heep.push(9)
+heep.push(4)
+heep.push(2)
+heep.push(3)
+heep.push(0)
+heep.push(2)
+heep.push(2)
+heep.push(7)
+print(heep.pop())
+print(heep.pop())
+print(heep.pop())
+print(heep.pop())
+print(heep.pop())
+print(heep.pop())
+print(heep.pop())
+print(heep.pop())
+print(heep.pop())
+print(heep.pop())
+print(heep.pop())
+print(heep.pop())
 
-heap = Heap()
-heap.push(1)
-heap.push(4)
-heap.push(2)
-heap.push(5)
-heap.push(3)
-heap.push(3)
-heap.push(3)
-heap.push(9)
-heap.push(8)
-heap.push(10)
-
-print(heap.pop())
-print(heap.pop())
-print(heap.pop())
-print(heap.pop())
-heap.push(100)
-print(heap.pop())
-print(heap.pop())
-print(heap.pop())
-print(heap.pop())
-print(heap.pop())
-print(heap.pop())
-print(heap.pop())
-print(heap.pop())
-print(heap.pop())
